b6d404ad9e0b035acf7b36b4c53539664bb8d768
Alexey Solovyov <hhblaze@gmail.com>
2018-02-23 10:17:48 +0100

    Adding Benchmark

---
+++ Benchmark/Objects/StateLogEntry.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmark.Objects
+{
+    /// <summary>
+    /// It's an operational class from https://github.com/hhblaze/Raft.Net/blob/master/Raft/StateMachine/StateLogEntry.cs
+    /// </summary>
+    [ProtoBuf.ProtoContract]
+    public class StateLogEntry : Biser.IEncoder
+    {
+        public StateLogEntry()
+        {
+            Term = 0;
+            Index = 0;
+            IsCommitted = false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>  
+        [ProtoBuf.ProtoMember(1, IsRequired = true)]
+        public ulong Term { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>        
+        [ProtoBuf.ProtoMember(2, IsRequired = true)]
+        public ulong Index { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary> 
+        [ProtoBuf.ProtoMember(3, IsRequired = true)]
+        public byte[] Data { get; set; }
+
+        /// <summary>
+        /// If value is committed by Leader
+        /// </summary>        
+        [ProtoBuf.ProtoMember(4, IsRequired = true)]
+        public bool IsCommitted { get; set; }
+
+        /// <summary>
+        /// Out of protobuf
+        /// </summary>        
+        [ProtoBuf.ProtoMember(5, IsRequired = true)]
+        public ulong PreviousStateLogId = 0;
+        /// <summary>
+        /// Out of protobuf
+        /// </summary>    
+        [ProtoBuf.ProtoMember(6, IsRequired = true)]
+        public ulong PreviousStateLogTerm = 0;
+
+        /// <summary>
+        /// RedirectId
+        /// </summary>
+        [ProtoBuf.ProtoMember(7, IsRequired = true)]
+        public ulong RedirectId = 0;
+        
+
+        #region "Biser"
+
+        public Biser.Encoder BiserEncoder(Biser.Encoder existingEncoder = null)
+        {
+            Biser.Encoder enc = new Biser.Encoder(existingEncoder);
+
+            enc
+            .Add(Term)
+            .Add(Index)
+            .Add(Data)
+            .Add(IsCommitted)
+            .Add(PreviousStateLogId)
+            .Add(PreviousStateLogTerm)
+            .Add(RedirectId)
+            ;
+            return enc;
+        }
+
+        public static StateLogEntry BiserDecode(byte[] enc = null, Biser.Decoder extDecoder = null) //!!!!!!!!!!!!!! change return type
+        {
+            Biser.Decoder decoder = null;
+            if (extDecoder == null)
+            {
+                if (enc == null || enc.Length == 0)
+                    return null;
+                decoder = new Biser.Decoder(enc);
+                if (decoder.CheckNull())
+                    return null;
+            }
+            else
+            {
+                decoder = new Biser.Decoder(extDecoder);
+                if (decoder.IsNull)
+                    return null;
+            }
+
+            StateLogEntry m = new StateLogEntry();  //!!!!!!!!!!!!!! change return type
+
+            m.Term = decoder.GetULong();
+            m.Index = decoder.GetULong();
+            m.Data = decoder.GetByteArray();
+            m.IsCommitted = decoder.GetBool();
+            m.PreviousStateLogId = decoder.GetULong();
+            m.PreviousStateLogTerm = decoder.GetULong();
+            m.RedirectId = decoder.GetULong();
+
+            return m;
+        }
+
+        #endregion
+    }
+}
---
+++ Benchmark/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Benchmark.Objects;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            t1();
+
+            Console.WriteLine("Press any key");
+            Console.ReadLine();
+        }
+
+
+        static void t1()
+        {
+            /*
+             Start
+                    Protobuf obj length: 22
+                    Biser obj length: 17
+                    Protobuf encode: 891 ms
+                    Protobuf decode: 1242 ms
+                    Biser encode: 523 ms
+                    Biser decode: 410 ms
+             Press any key
+             */
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            StateLogEntry obj = new StateLogEntry()
+            {
+                Data = new byte[] { 1, 2, 3, 4, 5 },
+                Index = 458,
+                IsCommitted = true,
+                PreviousStateLogId = 4789,
+                PreviousStateLogTerm = 447,
+                RedirectId = 12,
+                Term = 99
+            };
+
+            //Protobuf. Warming up, getting length
+            var pBt = obj.SerializeProtobuf();
+            Console.WriteLine($"Protobuf obj length: {pBt.Length}");
+            var pObj = pBt.DeserializeProtobuf<StateLogEntry>();
+
+            //Biser. Getting length
+            var bBt = new Biser.Encoder().Add(obj).Encode();
+            Console.WriteLine($"Biser obj length: {bBt.Length}");            
+            var bObj = StateLogEntry.BiserDecode(bBt);
+
+            byte[] tbt = null;
+            StateLogEntry tobj = null;
+
+            sw.Start();
+            for (int i=0;i<1000000;i++)
+            {
+                tbt = obj.SerializeProtobuf();
+            }
+            sw.Stop();
+            Console.WriteLine($"Protobuf encode: {sw.ElapsedMilliseconds} ms");
+            sw.Reset();
+
+            sw.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                tobj = pBt.DeserializeProtobuf<StateLogEntry>();
+            }
+            sw.Stop();
+            Console.WriteLine($"Protobuf decode: {sw.ElapsedMilliseconds} ms");
+            sw.Reset();
+
+            sw.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                tbt = new Biser.Encoder().Add(obj).Encode();
+            }
+            sw.Stop();
+            Console.WriteLine($"Biser encode: {sw.ElapsedMilliseconds} ms");
+            sw.Reset();
+
+            sw.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                tobj = StateLogEntry.BiserDecode(bBt);
+            }
+            sw.Stop();
+            Console.WriteLine($"Biser decode: {sw.ElapsedMilliseconds} ms");
+            sw.Reset();
+
+        }
+    }
+}
