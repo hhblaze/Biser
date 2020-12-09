105aadbf5b70540c1475ee61fefc7f120d631150
Alexey Solovyov <hhblaze@gmail.com>
2020-12-09 06:49:29 +0100

    Release BiserObjectify

--- Deployment/BiserObjectify/!!!Build.bat
+++ Deployment/BiserObjectify/!!!Build.bat
@@ -2,4 +2,4 @@
 rem "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe" "%~dp0..\Biser\BiserObjectify\BiserObjectify.csproj" /t:rebuild /p:Configuration=Release-NET47
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe" "%~dp0..\..\BiserObjectify_Standard\BiserObjectify_Standard.csproj" /t:rebuild /p:Configuration=Release
 
-nuget.exe pack "%~dp0!!!BiserObjectify.nuspec" -BasePath "%~dp0..\.." -OutputDirectory "%~dp0\"
+"%~dp0..\nuget.exe" pack "%~dp0!!!BiserObjectify.nuspec" -BasePath "%~dp0..\.." -OutputDirectory "%~dp0\"
